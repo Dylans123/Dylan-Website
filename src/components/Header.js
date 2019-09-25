4d7408c409ce36767593f47173a99c580bd0ef7b
dylans123 <dylanskelly@knights.ucf.edu>
2019-09-24 23:58:36 -0400

    Added mobile responsiveness and updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import dylan from '../assets/dylan-image.jpg';
 import linkedin from '../assets/linkedin-logo.svg';
 import email from '../assets/envelope.svg';
 import resume from '../assets/file.svg';
+import github from '../assets/github-logo.svg';
 import pdf from '../assets/resume.pdf';
 
 class Header extends React.Component {
@@ -18,31 +19,38 @@ class Header extends React.Component {
                 <div>
                     <img src={dylan} className="mainImg" />
                 </div>
-                <div className="mt-4" style={{fontSize: '50px', fontWeight: 'bold', textAlign: 'center', color: '#BDABE5'}}>
+                <div className="mt-4" style={{fontSize: '60px', width: '95%', fontWeight: 'bold', textAlign: 'center', color: '#BDABE5'}}>
                     Hey, I'm Dylan Skelly.
                 </div>
-                <div style={{fontSize: '40px', width: '85%', textAlign: 'center', color: '#BDABE5'}}>
+                <div style={{fontSize: '50px', width: '95%', fontWeight: 'bold', textAlign: 'center', color: '#BDABE5'}}>
                     Glad you're here!
                 </div>
                 <div className="row text-center my-4">
-                    <div className="col">
+                    <div className="col-sm-12 col-md">
                         <div className="btn">
                             <a href={pdf} target="_blank" download="Dylan Skelly Resume.pdf">
-                                <img src={resume} style={{width: '50px', height: '50px'}}/>
+                                <img className="btn-img" title="resume" alt-text="resume" src={resume}/>
                             </a>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="col-sm-12 col-md">
                         <div className="btn">
                             <a href="mailto: dylanskelly@knights.ucf.edu">
-                                <img src={email} style={{width: '50px', height: '50px'}}/>
+                                <img className="btn-img" title="email" alt-text="email" src={email}/>
                             </a>
                         </div>
                     </div>
-                    <div className="col" onClick="www.linkedin.com/in/dylan-skelly">
+                    <div className="col-sm-12 col-md">
+                        <div className="btn">
+                            <a href="https://github.com/Dylans123">
+                                <img className="btn-img" title="github" alt-text="github" src={github}/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md" onClick="www.linkedin.com/in/dylan-skelly">
                         <div className="btn">
                             <a href="https://www.linkedin.com/in/dylan-skelly">
-                                <img src={linkedin} style={{width: '50px', height: '50px'}}/>
+                                <img className="btn-img" title="linkedin" alt-text="linkedin" src={linkedin}/>
                             </a>
                         </div>
                     </div>
